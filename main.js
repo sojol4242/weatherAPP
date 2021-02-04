714eada22f25187265d542ccb717b16ac876d6ad
sojol4242 <bsmrstueee90@gmail.com>
2021-02-04 22:37:43 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+//  const input = document.getElementById("input")
+//  const button = document.getElementById("button")
+//  button.addEventListener('click', () => {
+//      if (input.value == '') {
+//          alert("Please Enter a City Name")
+//      } else {
+//          fetch(`api.openweathermap.org/data/2.5/weather?q=${input.value}&metric&appid=2f61081082d78a32eb6ee39341d20a5a`)
+//      }
+//  })
+
+document.getElementById("button").addEventListener("click", function() {
+    const input = document.getElementById("input").value;
+    // API CALL
+
+    if (input == '') {
+        alert("Please enter a city name")
+    } else {
+        fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=2f61081082d78a32eb6ee39341d20a5a`
+            )
+            .then(response => response.json())
+            .then((data) => {
+                const temperature = Math.floor(((data.main.temp) - 273))
+                console.log(temperature)
+                document.getElementById("city").innerText = data.name;
+                document.getElementById("temp").innerText = temperature;
+                document.getElementById("lead").innerText = data.weather[0].main;
+                document.getElementById("wind").innerText = data.wind.speed;
+                document.getElementById(
+                    "icon"
+                ).src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+                document.getElementById("error").innerText = "";
+                console.log(data);
+            })
+            .catch(err => {
+                // alert("Can't find a valid city")
+                document.getElementById("input").value = ''
+                document.getElementById("city").innerText = ''
+                document.getElementById("temp").innerText = '';
+                document.getElementById("lead").innerText = '';
+                document.getElementById("wind").innerText = null;
+                document.getElementById(
+                    "icon"
+                ).src = '';
+
+                document.getElementById("error").innerText = "We Can't find a valid city name.Please try again";
+            })
+
+    }
+});
